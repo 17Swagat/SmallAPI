6f60699fb719edc4f8754c46ff24f15ee46fba79
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2024-06-01 17:50:21 +0530

    learn 1: 3

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ app = FastAPI()
 # first "path" match gets selected
 # Hence ordering is important
 
+@app.get('/')
+def root():
+    return {'message': 'yo homie'}
+
 @app.get('/login')
 def get_user():
     return {'message': 'Hello world'}
