036667c7cd8f5da08e545db5ed06baa461c44423
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2024-06-01 17:32:54 +0530

    learn 1: api path ordering matters. app pt-3

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# first "path" match gets selected
+# Hence ordering is important
+
 @app.get('/login')
 def get_user():
     return {'message': 'Hello world'}
+
+@app.get('/posts')
+def get_post():
+    return {'data': 'this is your post'}
