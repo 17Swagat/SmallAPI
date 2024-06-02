d697e9957bcb0da3565d30d6719919a44050f947
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2024-06-02 19:36:02 +0530

    learn 1: storing in array

--- main.py
+++ main.py
@@ -10,19 +10,25 @@ class Post(BaseModel):
     published: bool = False
     rating: Optional[int] = 15
 
+my_posts = [{'title': 'title post 1', 'content': 'content post 1', 'id': 1}, {'title': 'favourite food', 'content': 'i like pizza', 'id': 2}]
+
 @app.get('/')
 def root():
     return {'message': 'yo homie'}
+
+@app.get('/posts')
+def get_post():
+    return {'data': my_posts}
 
 @app.get('/login')
 def get_user():
     return {'message': 'Hello world'}
 
-@app.get('/posts')
-def get_post():
-    return {'data': 'this is your post'}
+# @app.post('/posts/create')
+# def create_posts(post:Post):
+#     return {'titlex': post.title, 'contentx': post.content, 'publishedx': post.published,\
+#             'rating':post.rating, 'pydentaic': post.model_dump(), 'post': post, 'type(post)': str(type(post))}
 
-@app.post('/posts/create')
-def create_posts(post:Post):
-    return {'titlex': post.title, 'contentx': post.content, 'publishedx': post.published,\
-            'rating':post.rating, 'pydentaic': post.model_dump(), 'post': post, 'type(post)': str(type(post))}
+@app.post('/posts')
+def create_posts(post: Post):
+    return {'data': post}
