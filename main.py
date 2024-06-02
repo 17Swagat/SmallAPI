96fd035acbb4026ec043a52c856cfa17e7d9ecd4
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2024-06-02 21:21:37 +0530

    Latest posts

--- main.py
+++ main.py
@@ -5,35 +5,49 @@ from random import randrange
 
 app = FastAPI()
 
+
 class Post(BaseModel):
     title: str
-    content: str 
+    content: str
     published: bool = False
     rating: Optional[int] = 15
 
-my_posts = [{'title': 'title post 1', 'content': 'content post 1', 'id': 1}, {'title': 'favourite food', 'content': 'i like pizza', 'id': 2}]
 
-@app.get('/')
+my_posts = [
+    {"title": "title post 1", "content": "content post 1", "id": 1},
+    {"title": "favourite food", "content": "i like pizza", "id": 2},
+]
+
+
+@app.get("/")
 def root():
-    return {'message': 'yo homie'}
+    return {"message": "yo homie"}
 
-@app.get('/posts')
+
+@app.get("/posts")
 def get_post():
-    return {'data': my_posts}
+    return {"data": my_posts}
 
-@app.get('/posts/{id}')
+
+@app.get("/posts/latest")
+def latest_posts():
+    posts = my_posts[: (len(my_posts) - 1)]
+    return {"latest": posts}
+
+
+@app.get("/posts/{id}")
 def get_post(id: int):
-    '''Getting a specific post'''
+    """Getting a specific post"""
     for post in my_posts:
-        if post['id'] == id:
-            return {'post': post}
-    return {'message': 'post not found'}
+        if post["id"] == id:
+            return {"post": post}
+    return {"message": "post not found"}
 
 
-@app.post('/posts')
+@app.post("/posts")
 def create_posts(post: Post):
     # saving into array
     post_dict = post.model_dump()
-    post_dict['id'] = randrange(0, 1000000)
+    post_dict["id"] = randrange(0, 1000000)
     my_posts.append(post_dict)
-    return {'post': post_dict}
+    return {"post": post_dict}
