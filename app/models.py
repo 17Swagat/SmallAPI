c999709147f6220863d7971805578211247402a1
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2024-06-10 14:28:20 +0530

    Auth:pt-1.1: Adding User Table

--- app/models.py
+++ app/models.py
@@ -12,3 +12,12 @@ class Post(Base):
     content = Column("post_content", String(100), nullable=False)
     published = Column("post_published", Boolean, server_default='TRUE', nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable = False, server_default=text('NOW()'))
+
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column("user_id", Integer, primary_key=True, nullable=False)
+    email = Column(name='email',type_=String(50), nullable=False, unique=True)
+    password = Column(name='password',type_=String(50), nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable = False, server_default=text('NOW()'))
